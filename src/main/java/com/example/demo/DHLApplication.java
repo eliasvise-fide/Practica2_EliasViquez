20883064d91990ad02e80104e98847d4cc3213dc
eliasvise <jviquez20768@ufide.ac.cr>
2024-11-04 00:22:47 -0600

    Web Publish

---
+++ src/main/java/com/example/demo/DHLApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DHLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DHLApplication.class, args);
+	}
+
+}
